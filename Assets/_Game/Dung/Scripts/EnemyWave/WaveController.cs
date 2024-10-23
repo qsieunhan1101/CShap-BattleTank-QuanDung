15d56bfbe29515739fae85c6e1b1cfaa721735da
DinDun <van.dinhdung.tkh@gmail.com>
2024-10-23 11:49:39 +0700

    23/10/2024 03
    Enemy wave logic

---
+++ Assets/_Game/Dung/Scripts/EnemyWave/WaveController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+{
+    [SerializeField] public List<GameObject> tankEnemy;
+    [SerializeField] public List<Transform> spawnsTransform;
+    [SerializeField] protected GameObject enemyPrefab;
+    [SerializeField] protected GameObject effectSpawn;
+    [SerializeField] protected float timeDestroyEffect;
+
+    private void Awake()
+    {
+        SpawnEnemy();
+    }
+    
+    protected virtual void SpawnEnemy()
+    {
+        for (int i = spawnsTransform.Count - 1; i >= 0; i--)
+        {
+            GameObject tank = Instantiate(enemyPrefab, spawnsTransform[i].position, spawnsTransform[i].rotation);
+            tank.transform.SetParent(spawnsTransform[i].gameObject.transform);
+            GameObject effect = Instantiate(effectSpawn, spawnsTransform[i].position, spawnsTransform[i].rotation);
+            tankEnemy.Add(tank);
+            Destroy(effect, timeDestroyEffect);
+            spawnsTransform.RemoveAt(i);
+        }
+    }
+    protected void ClearEnemy()
+    {
+        for (int i = 0; i < tankEnemy.Count; i++)
+        {
+            if (tankEnemy[i] == null)
+            {
+                tankEnemy.RemoveAt(i);
+            }
+        }
+        if (tankEnemy.Count == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void Update()
+    {
+        ClearEnemy();
+    }
+}
