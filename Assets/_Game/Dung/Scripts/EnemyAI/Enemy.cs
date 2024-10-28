ddcae8ab38b303e5d5ab2fecad6603f13ecb199c
DinDun <van.dinhdung.tkh@gmail.com>
2024-10-28 16:54:50 +0700

    Update Enemy.cs

--- Assets/_Game/Dung/Scripts/EnemyAI/Enemy.cs
+++ Assets/_Game/Dung/Scripts/EnemyAI/Enemy.cs
@@ -11,11 +11,12 @@ using static UnityEngine.GraphicsBuffer;
 
 public class Enemy : MonoBehaviour
 {
+    #region variable
     IState currentState;
     [Header("Character_Enemy")]
     [SerializeField] private Rigidbody rb;
     [SerializeField] Animator anim;
-    public bool IsDead;
+    public bool IsDead = false;
 
     [Header("Pool")]
     [SerializeField] private GameObject bulletBasePrefab;
@@ -40,6 +41,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float maxHp;
 
     #endregion
+
     private void Start()
     {
         SetUpHealSlider();
@@ -144,11 +146,16 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    private void Die()
+    [ContextMenu("Enemy Die")]
+    public void Die()
     {
+        if (IsDead) return;
+
+        IsDead = true;
         if (OnDestroyed != null)
         {
             OnDestroyed.Invoke();
+            Destroy(gameObject);
         }
     }
 
